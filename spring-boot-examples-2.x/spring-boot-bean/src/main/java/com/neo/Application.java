a0000c6445ad72e2554f358b29c762d266cabf83
user3 <1014780649@qq.com>
2024-04-30 10:03:54 +0800

    学习spring boot @Slf4j

--- spring-boot-examples-2.x/spring-boot-bean/src/main/java/com/neo/Application.java
+++ spring-boot-examples-2.x/spring-boot-bean/src/main/java/com/neo/Application.java
@@ -3,11 +3,13 @@ package com.neo;
 import com.neo.bean.*;
 import com.neo.controller.HelloWorldController;
 import com.neo.util.SpringUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@Slf4j// 使用lombok,起到了便捷的作用
 public class Application {
 
 	public static void main(String[] args) {
@@ -19,6 +21,12 @@ public class Application {
 		System.out.println(applicationContext.getBean(ComponentExample.class));
 		System.out.println(applicationContext.getBean(CustomComponentExample.class));
 		System.out.println(applicationContext.getBean(HelloWorldController.class));
+		log.info(applicationContext.getBean(ControllerExample.class).toString());
+		log.info(applicationContext.getBean(ServiceExample.class).toString());
+		log.info(applicationContext.getBean(RepositoryExample.class).toString());
+		log.info(applicationContext.getBean(ComponentExample.class).toString());
+		log.info(applicationContext.getBean(CustomComponentExample.class).toString());
+		log.info(applicationContext.getBean(HelloWorldController.class).toString());
 		System.out.println("end Hello, World!");
 	}
 }
